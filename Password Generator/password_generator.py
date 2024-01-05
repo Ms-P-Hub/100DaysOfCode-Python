377151f2628b4b09f4c45b8980d5fd95128c851c
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-05 10:43:42 +0200

    Day 5 Done

---
+++ Password Generator/password_generator.py
@@ -0,0 +1,32 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+l_sequence = ""
+for i in range(0,nr_letters):
+    l_sequence += letters[random.randint(0,26)]
+    
+n_sequence = ""
+for i in range(0,nr_numbers):
+    n_sequence += numbers[random.randint(0,9)]
+
+s_sequence = ""
+for i in range(0,nr_symbols):
+    s_sequence += symbols[random.randint(0,9)]
+    
+password = l_sequence+n_sequence+s_sequence
+
+print(f"Easy Password : {password}")
+
+password_list = list(password)
+random.shuffle(password_list)
+print(f"Hard Password {''.join(password_list)}")
+    
+
