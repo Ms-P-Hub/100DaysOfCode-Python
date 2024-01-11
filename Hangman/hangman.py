7dba4b04ac7e3d3e6eb6711167cddb3be7f26239
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-11 23:35:58 +0200

    Detects wrong or right

---
+++ Hangman/hangman.py
@@ -0,0 +1,87 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+                   
+print(logo)             
+word_list = ["aardvark", "baboon", "camel"]
+
+import random
+chosen_word = random.choice(word_list)
+
+list_of_word =  list(chosen_word)
+hidden_list = []
+
+for i in list_of_word:
+  hidden_list.append("_ ")
+
+guess = input("\nGuess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+      print("Right")
+    else:
+        print("Wrong")
+        
