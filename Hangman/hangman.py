72f683b18dd5943d9e6570da52272088c3811cb3
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-12 15:06:33 +0200

    Worked on user experience

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,37 +1,43 @@
 import random
 import hangman_art as art
+import hangman_words as words
                    
 print(art.logo)   
           
-word_list = ["aardvark", "baboon", "camel"]
+word_list = words.word_list
 
 chosen_word = random.choice(word_list)
 
 list_of_word =  list(chosen_word)
+
 hidden_list = []
 
 for i in list_of_word:
   hidden_list.append("_")
   
-print(chosen_word)
 
 lives = 6
 
 while lives != 0:
+  print(" ".join(hidden_list))
   guess = input("\nGuess a letter: ").lower()
+  
+  if guess in hidden_list:
+      print("Opps! You've already guessed that letter! Try again") 
 
   for i in range(len(chosen_word)):
     if chosen_word[i] == guess:
       hidden_list[i] = guess
-      
+    
   if "_" not in hidden_list:
     print("You Win!")
     break
-  else:
-    print(" ".join(hidden_list))
-   
-  if guess not in list_of_word:
+  
+  elif guess not in list_of_word:
     lives -=1
+    print(f"{guess} is not in the word. You have {lives} lives remaining.")
     print(art.stages[lives])
     if lives == 0:
-      print(f"You Lose!\nThe word was {chosen_word}")
+      print(f"You Lose!\nThe word was {chosen_word}.")
+  
+  
