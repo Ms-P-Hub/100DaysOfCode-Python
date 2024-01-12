d58af227c5d8b44fdcf27d7b548c2548189e60dd
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-12 16:55:52 +0200

    Day 8 - fixed user experience

--- Caesar Cipher/caesar_cipher.py
+++ Caesar Cipher/caesar_cipher.py
@@ -27,43 +27,59 @@ alphabet = [
     "z",
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+option = False
+
+while option == False :
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
-    alphabet_shift = []
-    encrypted_text = []
+    def encrypt(text, shift):
+        alphabet_shift = []
+        encrypted_text = []
 
-    for i in range((shift + 1), len(alphabet)):
-        alphabet_shift.append(alphabet[i])
+        for i in range((shift + 1), len(alphabet)):
+            alphabet_shift.append(alphabet[i])
 
-    for i in range(shift + 1):
-        alphabet_shift.append(alphabet[i])
+        for i in range(shift + 1):
+            alphabet_shift.append(alphabet[i])
 
-    for letter in text:
-        location = alphabet.index(letter)
-        encrypted_text.append(alphabet_shift[location - 1])
+        for letter in text:
+            location = alphabet.index(letter)
+            encrypted_text.append(alphabet_shift[location - 1])
 
-    print(f"The encoded text is '{''.join(encrypted_text)}'.")
+        print(f"The encoded text is '{''.join(encrypted_text)}'.")
 
 
-def decrypt(text, shift):
-    alphabet_shift = []
-    decrypted_text = []
+    def decrypt(text, shift):
+        alphabet_shift = []
+        decrypted_text = []
 
-    for i in range(len(alphabet) - 1, (shift + 1), -1):
-        alphabet_shift.append(alphabet[i])
+        for i in range(len(alphabet) - 1, (shift + 1), -1):
+            alphabet_shift.append(alphabet[i])
 
-    for i in range(shift - 1, -1, -1):
-        alphabet_shift.append(alphabet[i])
+        for i in range(shift - 1, -1, -1):
+            alphabet_shift.append(alphabet[i])
 
-    for letter in text:
-        location = alphabet.index(letter)
-        decrypted_text.append(alphabet_shift[location-1])
+        for letter in text:
+            location = alphabet.index(letter)
+            decrypted_text.append(alphabet_shift[location-1])
+            
+        print(f"The decoded text is '{''.join(decrypted_text)}'.")
+
+    if direction == 'encode':
+        encrypt(text, shift)
+    elif direction == 'decode':
+        decrypt(text, shift)
+    else:
+        print("Invalid option")
         
-    print(f"The decoded text is '{''.join(decrypted_text)}'.")
-
-
-decrypt(text, shift)
+    option = input("Would you like to start over? Type 'yes' or 'no'.\n").lower()
+    
+    if option == "yes" :
+        option = False
+    elif option == "no":
+        option = True
+    else:
+        print("Invalid option")
