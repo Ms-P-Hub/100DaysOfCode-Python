1f315b7c86ce3bde4a6d0eaee2e5b2696f24ddf4
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-14 00:41:55 +0200

    Day 9 - Max amount calculated

--- Secret Auction/secret_auction.py
+++ Secret Auction/secret_auction.py
@@ -5,6 +5,17 @@ print(logo)
 bids = {}
 another_bid = True
 
+def find_highest_bidder(library):
+    highest_amount = 0
+    
+    for key in bids:
+        value = bids[key]
+        if value > highest_amount:
+            highest_amount = value
+            highest_bidder = key
+    
+    print(f"The winner is {highest_bidder} with a bid of R{highest_amount}")
+
 while another_bid:
     name = input("What is your name: ").capitalize()
     amount = int(input("What's your bid: R"))
@@ -17,3 +28,7 @@ while another_bid:
         os.system('clear')
     else:
         another_bid = False
+        find_highest_bidder(bids)
+
+
+    
