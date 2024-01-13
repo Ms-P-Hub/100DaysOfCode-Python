99714bce20c059d84d2a7487f571bc50413143ab
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-13 21:40:22 +0200

    Day 9 - first attempt

---
+++ Secret Auction/secret_auction.py
@@ -0,0 +1,17 @@
+import os
+
+bids = {}
+another_bid = True
+
+while another_bid:
+    name = input("What is your name: ").capitalize()
+    amount = input("What's your bid: R")
+    
+    bids[name] = amount
+    
+    more_bidders =  input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    if more_bidders == 'yes':
+        another_bid = True
+        os.system('clear')
+    else:
+        another_bid = False
