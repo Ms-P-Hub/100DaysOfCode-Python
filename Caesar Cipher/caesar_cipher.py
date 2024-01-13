7ab432b2b84bf8a5a1601366fe92ff23cd7bb5be
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-13 21:02:34 +0200

    Day 8 - screen clears

--- Caesar Cipher/caesar_cipher.py
+++ Caesar Cipher/caesar_cipher.py
@@ -1,43 +1,73 @@
 from art import logo
+import os
 
-alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
 
 print(logo)
+
+
 def caesar(start_text, shift, direction):
-        alphabet_shift = []
-        text = []
+    alphabet_shift = []
+    text = []
 
-        if direction == "encode":
-            for i in range((shift + 1), len(alphabet)):
-                alphabet_shift.append(alphabet[i])
+    if direction == "encode":
+        for i in range((shift + 1), len(alphabet)):
+            alphabet_shift.append(alphabet[i])
 
-            for i in range(shift + 1):
-                alphabet_shift.append(alphabet[i])
+        for i in range(shift + 1):
+            alphabet_shift.append(alphabet[i])
 
-            for letter in start_text:
-                if not letter.isalpha():
-                    text.append(letter)
-                else:
-                    location = alphabet.index(letter)
-                    text.append(alphabet_shift[location - 1])
+        for letter in start_text:
+            if not letter.isalpha():
+                text.append(letter)
+            else:
+                location = alphabet.index(letter)
+                text.append(alphabet_shift[location - 1])
 
-            print(f"The encoded text is '{''.join(text)}'.")
+        print(f"The encoded text is '{''.join(text)}'.")
 
-        elif direction == "decode":
-            for i in range((len(alphabet) - shift), len(alphabet)):
-                alphabet_shift.append(alphabet[i])
+    elif direction == "decode":
+        for i in range((len(alphabet) - shift), len(alphabet)):
+            alphabet_shift.append(alphabet[i])
 
-            for i in range((len(alphabet) - shift)):
-                alphabet_shift.append(alphabet[i])
+        for i in range((len(alphabet) - shift)):
+            alphabet_shift.append(alphabet[i])
 
-            for letter in start_text:
-                if not letter.isalpha():
-                    text.append(letter)
-                else:
-                    location = alphabet.index(letter)
-                    text.append(alphabet_shift[location])
+        for letter in start_text:
+            if not letter.isalpha():
+                text.append(letter)
+            else:
+                location = alphabet.index(letter)
+                text.append(alphabet_shift[location])
 
-            print(f"The decoded text is '{''.join(text)}'.")
+        print(f"The decoded text is '{''.join(text)}'.")
 
 
 option = False
@@ -49,13 +79,14 @@ while not option:
 
     if shift > 26:
         shift = shift % 26
-   
+
     caesar(text, shift, direction)
 
     option = input("Would you like to start over? Type 'yes' or 'no'.\n").lower()
 
     if option == "yes":
         option = False
+        os.system("clear")
     elif option == "no":
         print("GoodBye!")
         option = True
