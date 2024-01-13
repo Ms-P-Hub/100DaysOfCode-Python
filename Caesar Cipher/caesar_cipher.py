8e975836cd42bbf35863286ed2b2d61dc36283e1
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-13 20:22:35 +0200

    Day 8 - Can take special chars

--- Caesar Cipher/caesar_cipher.py
+++ Caesar Cipher/caesar_cipher.py
@@ -29,18 +29,16 @@ alphabet = [
 
 option = False
 
-while option == False:
+while not option:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    
+
     def caesar(start_text, shift, direction):
-        
         alphabet_shift = []
         text = []
-        
-        if direction == "encode":
 
+        if direction == "encode":
             for i in range((shift + 1), len(alphabet)):
                 alphabet_shift.append(alphabet[i])
 
@@ -48,24 +46,31 @@ while option == False:
                 alphabet_shift.append(alphabet[i])
 
             for letter in start_text:
-                location = alphabet.index(letter)
-                text.append(alphabet_shift[location - 1])
+                if not letter.isalpha():
+                    text.append(letter)
+                else:
+                    location = alphabet.index(letter)
+                    text.append(alphabet_shift[location - 1])
 
             print(f"The encoded text is '{''.join(text)}'.")
-        
-        elif direction == "decode":
 
-            for i in range((len(alphabet)- shift), len(alphabet)):
+        elif direction == "decode":
+            for i in range((len(alphabet) - shift), len(alphabet)):
                 alphabet_shift.append(alphabet[i])
 
-            for i in range((len(alphabet)- shift)):
+            for i in range((len(alphabet) - shift)):
                 alphabet_shift.append(alphabet[i])
 
             for letter in start_text:
-                location = alphabet.index(letter)
-                text.append(alphabet_shift[location])
+                if not letter.isalpha():
+                    text.append(letter)
+                else:
+                    location = alphabet.index(letter)
+                    text.append(alphabet_shift[location])
 
             print(f"The decoded text is '{''.join(text)}'.")
+
+    caesar(text, shift, direction)
 
     option = input("Would you like to start over? Type 'yes' or 'no'.\n").lower()
 
