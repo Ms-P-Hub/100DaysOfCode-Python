adfe1c124a27d475f07b728da9298e6a73f548b2
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-18 14:26:22 +0200

    Day 10 - Loops

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,24 +1,36 @@
 def add(a, b):
     return a + b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     return a / b
 
-def subtract(a ,b):
+
+def subtract(a, b):
     return a - b
 
-operators = {"+" : add, "*" : multiply, "/" : divide, "-" : subtract}
 
+operators = {"+": add, "*": multiply, "/": divide, "-": subtract}
+again = True
 num1 = int(input("Enter the first number: "))
-for key in operators:
-    print(key)
-operator = input("Pick an operation from the line above: ")
-num2 = int(input("Enter the second number: "))
 
-equation = operators[operator]
-answer = equation(num1,num2)
+while again:
+    for key in operators:
+        print(key)
+    operator = input("Pick an operation from the line above: ")
+    num2 = int(input("Enter the next number: "))
 
-print(f"{num1} {operator} {num2} = {answer}")
+    equation = operators[operator]
+    answer = equation(num1, num2)
+
+    print(f"{num1} {operator} {num2} = {answer}")
+
+    should_continue =input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ").lower()
+    
+    if should_continue == "y":
+        num1 = answer
+        again = True
