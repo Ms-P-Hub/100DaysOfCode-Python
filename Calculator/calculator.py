ae44de0d4a7ff5356a4a2a0f41fbf3b788221404
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-18 14:33:59 +0200

    Day 10 - Recursion

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -13,24 +13,31 @@ def divide(a, b):
 def subtract(a, b):
     return a - b
 
-
 operators = {"+": add, "*": multiply, "/": divide, "-": subtract}
-again = True
-num1 = int(input("Enter the first number: "))
 
-while again:
-    for key in operators:
-        print(key)
-    operator = input("Pick an operation from the line above: ")
-    num2 = int(input("Enter the next number: "))
 
-    equation = operators[operator]
-    answer = equation(num1, num2)
+def calculation():
+    again = True
+    num1 = int(input("Enter the first number: "))
 
-    print(f"{num1} {operator} {num2} = {answer}")
+    while again:
+        for key in operators:
+            print(key)
+        operator = input("Pick an operation from the line above: ")
+        num2 = int(input("Enter the next number: "))
 
-    should_continue =input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ").lower()
-    
-    if should_continue == "y":
-        num1 = answer
-        again = True
+        equation = operators[operator]
+        answer = equation(num1, num2)
+
+        print(f"{num1} {operator} {num2} = {answer}")
+
+        should_continue =input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit s to start a new calculation: ").lower()
+        
+        if should_continue == "y":
+            num1 = answer
+            again = True
+        elif should_continue == 's':
+            calculation()
+        else:
+            break
+calculation()
