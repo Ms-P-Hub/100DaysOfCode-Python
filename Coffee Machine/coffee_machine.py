46e469c8f3422f916e4b97a1c03d83468486d232
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-24 15:57:33 +0200

    Day 14 - Payments done

---
+++ Coffee Machine/coffee_machine.py
@@ -0,0 +1,87 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 12,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 13,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 15,
+    },
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+money = 0
+
+
+def machine_on():
+    choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+
+    if choice == "report":
+        print(
+            f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}"
+        )
+    elif choice == "latte":
+        check_resources(choice)
+    elif choice == "espresso":
+        check_resources(choice)
+    elif choice == "cappuccino":
+        check_resources(choice)
+    machine_on()
+
+
+def payment(choice):
+    due = MENU[choice]["cost"]
+    print("Please insert coins.")
+    one = float(input("How many R1: "))
+    two = float(input("How many R2: "))
+    five = float(input("How many R5: "))
+
+    amount = one + (two * 2) + (five * 5)
+
+    if amount >= due:
+        print(f"Here is R{amount - due} in change.")
+        global money
+        money += due
+    else:
+        print(f"Money is sort with R{due - amount}")
+        payment(choice)
+
+
+def check_resources(choice):
+    if resources["coffee"] < MENU[choice]["ingredients"]["coffee"]:
+        print("Running low on Coffee. Sorry.")
+    elif resources["water"] < MENU[choice]["ingredients"]["water"]:
+        print("Running low on Water. Sorry.")
+    elif resources["milk"] < MENU[choice]["ingredients"]["milk"]:
+        print("Running low on Coffee. Sorry.")
+    else:
+        payment(choice)
+        resources["coffee"] = (
+            resources["coffee"] - MENU[choice]["ingredients"]["coffee"]
+        )
+        resources["water"] = resources["water"] - MENU[choice]["ingredients"]["water"]
+        resources["milk"] = resources["milk"] - MENU[choice]["ingredients"]["milk"]
+        print(f"Here is your {choice}☕ Enjoy!")
+
+
+machine_on()
