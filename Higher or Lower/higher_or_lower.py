b44dff5550f3578943f8848c773ebeb396c074ec
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-24 14:53:42 +0200

    Day 14 - fixed description

--- Higher or Lower/higher_or_lower.py
+++ Higher or Lower/higher_or_lower.py
@@ -7,8 +7,8 @@ def choice():
 
 print(logo)
 choice_1 = choice()
-print(f"Compare A: {choice_1['name']}, {choice_1['description']} from {choice_1['country']}")
+print(f"Compare A: {choice_1['name']}, a {choice_1['description']} from {choice_1['country']}")
 
 print(vs)
 choice_2 = choice()
-print(f"Compare B: {choice_2['name']}, {choice_2['description']} from {choice_2['country']}")
+print(f"Compare B: {choice_2['name']}, a {choice_2['description']} from {choice_2['country']}")
