dbf178973c21b528c53194cdbd583282ead45a86
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-24 14:33:42 +0200

    Day 12 - art added

--- Guess The Number/guess_the_number.py
+++ Guess The Number/guess_the_number.py
@@ -1,25 +1,29 @@
 import random
+from art import logo
 
+print(logo)
 print("Welcome to the Guessing Game!\nI'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
-random_number = random.randint(1,100)
+random_number = random.randint(1, 100)
+
 
 def easy():
     guesses = 10
     core_functionality(guesses)
-        
+
+
 def hard():
     guesses = 5
     core_functionality(guesses)
-    
-    
+
+
 def core_functionality(guesses):
     print(f"You have {guesses} attempts remaining to guess the number.")
     while guesses > 0:
         guess = int(input("Make a guess: "))
-        
-        if guess > random_number :
+
+        if guess > random_number:
             print("Too High\nGuess again")
             guesses -= 1
         elif guess < random_number:
@@ -30,10 +34,11 @@ def core_functionality(guesses):
             break
         print(f"You have {guesses} attempts remaining to guess the number.")
     print(f"You ran out of guesses. You lose! The answer was {random_number}")
-        
-if difficulty == 'easy':
+
+
+if difficulty == "easy":
     easy()
-elif difficulty == 'hard':
+elif difficulty == "hard":
     hard()
 else:
     print("Invalid Input")
