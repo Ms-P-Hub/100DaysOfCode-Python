ad5db483cbeca15bc08291b1f9b618c55170900b
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-25 09:40:10 +0200

    Day 16 - Done

--- Coffee Machine OOP/main.py
+++ Coffee Machine OOP/main.py
@@ -3,22 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 from art import logo
+
 coffee = CoffeeMaker()
 money = MoneyMachine()
 menu = Menu()
-# menu_item = MenuItem()
 
-print(logo)
-choice = input(f"What would you like? {menu.get_items()}: ").lower()
+is_on = True
 
-if choice == "report":
-    coffee.report()
-    money.report()
-# elif coffee.is_resource_sufficient(choice):
-    
-#     MoneyMachine.make_payment()
-#     CoffeeMaker.make_coffee(choice)
-    
+while is_on:
+    print(logo)
+    choice = input(f"What would you like? {menu.get_items()}: ").lower()
 
-
-
+    if choice == 'off':
+        is_on = False
+    elif choice == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            coffee.make_coffee(drink)
