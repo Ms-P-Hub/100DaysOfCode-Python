b3961b1ef3a8af30a769e6256bed1fd102c96da0
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-25 09:23:16 +0200

    Day 16 - Report Works

--- Coffee Machine OOP/main.py
+++ Coffee Machine OOP/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+from art import logo
+coffee = CoffeeMaker()
+money = MoneyMachine()
+menu = Menu()
+# menu_item = MenuItem()
+
+print(logo)
+choice = input(f"What would you like? {menu.get_items()}: ").lower()
+
+if choice == "report":
+    coffee.report()
+    money.report()
+# elif coffee.is_resource_sufficient(choice):
+    
+#     MoneyMachine.make_payment()
+#     CoffeeMaker.make_coffee(choice)
+    
+
+
+
