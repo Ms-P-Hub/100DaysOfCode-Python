42f0bbe92e31e3dd5d9b932f1ff8ed22469b40e6
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-25 15:08:59 +0200

    Day 17 - Question model completed

---
+++ Quiz project/main.py
@@ -0,0 +1,10 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank = []
+for question in question_data:
+    new_question = Question(question["text"],question["answer"])
+    question_bank.append(new_question)
+    
+QuizBrain(question_bank)
