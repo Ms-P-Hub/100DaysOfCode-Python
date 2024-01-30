f0680d2b54c725deb6c04798cb00ad0e7985b40e
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 03:45:31 +0200

    Day 21 - Both paddle created

--- Pong/main.py
+++ Pong/main.py
@@ -2,22 +2,26 @@ from turtle import Screen
 from paddle import Paddle
 import turtle
 
+is_game_on = True
+
 screen = Screen()
 screen.setup(800, 600, 0, 0)
 screen.bgcolor("black")
 screen.title("Pong!")
 screen.tracer(0)
 
-paddle = Paddle()
-
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
 
 screen.listen()
-screen.onkeypress(paddle.up, "Up")
-screen.onkeypress(paddle.down, "Down")
+screen.onkeypress(right_paddle.up, "Up")
+screen.onkeypress(right_paddle.down, "Down")
 
-is_game_on = True
+screen.onkeypress(left_paddle.up, "w")
+screen.onkeypress(left_paddle.down, "s")
+
 
 while is_game_on:
     screen.update()
-    
+
 screen.exitonclick()
--- Pong/paddle.py
+++ Pong/paddle.py
@@ -2,17 +2,16 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, x_position, y_position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.turtlesize(5, 1, 0)
         self.penup()
-        
-        self.goto(350, 0)
+        self.goto(x_position, y_position)
 
     def up(self):
-        self.goto(self.xcor(),self.ycor()+20)
+        self.goto(self.xcor(), self.ycor() + 20)
 
     def down(self):
-        self.goto(self.xcor(),self.ycor()-20)
+        self.goto(self.xcor(), self.ycor() - 20)
