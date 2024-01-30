770d2b930c88f0a22728aa9037795bd025b561ff
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 06:14:00 +0200

    Day 23 - Screen had been set

---
+++ Turtle Crossing/main.py
@@ -0,0 +1,18 @@
+from turtle import Screen
+import time
+
+screen = Screen()
+screen.setup(600,600)
+screen.tracer(0)
+
+
+is_game_on = True
+
+while is_game_on:
+    time.sleep(0.1)
+    screen.update
+
+
+
+
+screen.exitonclick()
