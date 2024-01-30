2c579dad3d1b291c76f1d6c51c8bba734815a1c5
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 03:34:56 +0200

    Day 21 - First paddle created

--- Pong/main.py
+++ Pong/main.py
@@ -1,11 +1,16 @@
 from turtle import Screen
+from paddle import Paddle
 import turtle
-from player import Player
 
 screen = Screen()
 screen.setup(800, 600, 0, 0)
 screen.bgcolor("black")
 screen.title("Pong!")
 
+paddle = Paddle()
+
+screen.listen()
+screen.onkeypress(paddle.up,"Up")
+screen.onkeypress(paddle.down,"Down")
 
 screen.exitonclick()
