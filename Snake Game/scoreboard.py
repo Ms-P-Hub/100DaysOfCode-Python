1bc88c08b3180904d6622f7a708c2943d1630055
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 11:33:14 +0200

    Day 24 - Highscore can be saved

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -4,7 +4,7 @@ from turtle import Turtle
 class Score(Turtle):
     def __init__(self):
         super().__init__()
-        self.highscore = 0
+        self.highscore = self.fetch_highscore()
         self.score = 0
         self.color("white")
         self.penup()
@@ -31,8 +31,16 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.update_highscore()
         self.score = 0
         self.update_score()
-            
-        
-        
+
+    def update_highscore(self):
+        with open("./Snake Game/highscore.txt", "w") as file:
+            file.write(str(self.highscore))
+
+    def fetch_highscore(self):
+        with open("./Snake Game/highscore.txt", "r") as file:
+            self.highscore = int(file.readline())
+
+        return self.highscore
