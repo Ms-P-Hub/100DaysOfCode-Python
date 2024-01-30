f569b4b503dbb4d6efedf3c6080d62d490b4844c
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 03:35:23 +0200

    Day 21 - paddle created

---
+++ Pong/paddle.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(5, 1, 0)
+        self.penup()
+        self.goto(350, 0)
+
+    def up(self):
+        self.goto(self.xcor(),self.ycor()+20)
+
+    def down(self):
+        self.goto(self.xcor(),self.ycor()-20)
