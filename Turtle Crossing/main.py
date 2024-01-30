116018dce2b60884d42872e09f561ed1ede6c3cc
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 09:43:54 +0200

    Day 23 - Player Class done

--- Turtle Crossing/main.py
+++ Turtle Crossing/main.py
@@ -1,18 +1,26 @@
 from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(600,600)
+screen.setup(600, 600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.up, "Up")
 
 is_game_on = True
 
 while is_game_on:
     time.sleep(0.1)
-    screen.update
+    screen.update()
 
-
+    if player.finished():
+        player.reset()
 
 
 screen.exitonclick()
