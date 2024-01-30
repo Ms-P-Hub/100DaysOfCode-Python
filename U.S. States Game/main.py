ae2229eef81681b119c15269a96b0e6ae4a2d6a4
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-31 00:19:56 +0200

    Day 25 - Done

---
+++ U.S. States Game/main.py
@@ -0,0 +1,33 @@
+from turtle import Screen, Turtle
+import pandas as pd
+
+score = 0
+
+game = Turtle()
+game.penup()
+game.hideturtle()
+
+screen = Screen()
+screen.setup(725, 491)
+screen.bgpic("./U.S. States Game/blank_states_img.gif")
+screen.title("U.S. States Game")
+
+data = pd.read_csv("./U.S. States Game/50_states.csv")
+states = data.state.to_list()
+
+is_game_on = True
+
+while is_game_on:
+    user_input = screen.textinput(f"{score}/{len(states)} States Correct","What's another state name?").title()
+
+
+    if user_input in states:
+        score += 1
+        x = int(data[data.state == user_input].x)
+        y = int(data[data.state == user_input].y)
+        game.goto(x , y)
+        game.write(user_input,move=False,font=("Arial",8,"normal"))
+        
+        
+    
+screen.exitonclick()
