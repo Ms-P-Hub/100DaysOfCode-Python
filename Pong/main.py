cab6cfa1f70e16a60ac279f4850561e530bfbdeb
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-01-30 03:05:50 +0200

    Day 22 - Screen setup done

---
+++ Pong/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+import turtle
+from player import Player
+
+screen = Screen()
+screen.setup(800, 600, 0, 0)
+screen.bgcolor("black")
+screen.title("Pong!")
+
+
+screen.exitonclick()
