4fea76470a24483f8d8eb43e17e705b35a131e88
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-01 01:36:56 +0200

    Day 27 - Converter done

---
+++ Mile to Kilometer converter/main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.config(padx=50,pady=50)
+
+    
+user = Entry(width=15)
+user.config(width=10)
+user.grid(column=1,row=0)
+
+label_1 = Label(text="Miles", font=("Arial", 10, "normal"))
+label_1.grid(column=2,row=0)
+
+label_2 = Label(text="is equal to", font=("Arial", 10, "normal"))
+label_2.grid(column=0,row=1)
+
+label_3 = Label(text="0", font=("Arial", 10, "normal"))
+label_3.config(width=10)
+label_3.grid(column=1,row=1)
+
+label_4 = Label(text="Km", font=("Arial", 10, "normal"))
+label_4.grid(column=2,row=1)
+      
+def button_pressed():
+    label_3.config(text=str(float(user.get()) * 1.6))
+    
+button = Button(text="Calculate",command=button_pressed)
+button.grid(column=1,row=2)
+
+
+    
+
+window.mainloop()
