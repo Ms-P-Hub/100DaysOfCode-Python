6b7b6dd145473a0f59332ac9a92c2a519ca7d2a2
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-08 14:43:37 +0200

    Day 28 - Labels work

--- Pomodoro App/main.py
+++ Pomodoro App/main.py
@@ -7,23 +7,47 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
+rep = 0
+
 # ------------------------------------------------------#
 def start_timer():
-    count_down(WORK_MIN*60)
-    
+    global rep
+
+    rep += 1
+    work = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+
+    if rep == 8:
+        count_down(long_break)
+        timer_label.config(fg=RED, text="LONG BREAK", bg=YELLOW, font=(FONT_NAME, 48, "bold"))
+    elif rep % 2 == 0:
+        count_down(short_break)
+        timer_label.config(fg=PINK, text="BREAK", bg=YELLOW, font=(FONT_NAME, 48, "bold"))
+    else:
+        count_down(work)
+        timer_label.config(fg=GREEN, text="WORK", bg=YELLOW, font=(FONT_NAME, 48, "bold"))
+
+
 def count_down(count):
-    
-    count_min = math.floor(count/60)
+    count_min = math.floor(count / 60)
     count_sec = count % 60
+
     if count_sec == 0:
         count_sec = "00"
-    
-    canvas.itemconfig(time_text,text=f"{count_min}:{count_sec}")
-    if count > 0 : window.after(1000,count_down,count-1)
-    
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+
+    canvas.itemconfig(time_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 # ------------------------------------------------------#
 window = Tk()
 window.title("Pomorodo Clock")
@@ -39,11 +63,15 @@ tick_label.grid(column=1, row=3)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 110, image=picture)
-time_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+time_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
 
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", bg=YELLOW, activebackground=GREEN,command=start_timer)
+start_button = Button(
+    text="Start", bg=YELLOW, activebackground=GREEN, command=start_timer
+)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", bg=YELLOW, activebackground=GREEN)
