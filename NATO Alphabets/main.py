4c8c65f305c610bb2f7c2f81079dd8c784a3cd79
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-10 14:22:35 +0200

    Day 30 - added error message

--- NATO Alphabets/main.py
+++ NATO Alphabets/main.py
@@ -4,12 +4,13 @@ def nato_alphabet():
     name = input("Enter Name: ").upper()       
 
     data = pandas.read_csv("./NATO Alphabets/nato_phonetic_alphabet.csv")
-
+    format_dict = {row['letter']:row['code'] for indGex, row in data.iterrows()}
+    
     try:
-        format_dict = {row['letter']:row['code'] for indGex, row in data.iterrows()}
         nato = [format_dict[i] for i in name]
         return print(nato)
-    except KeyError(""):
+    except KeyError:
+        print("Sorry. Only accepts alphabets")
         nato_alphabet()
 
 nato_alphabet()
