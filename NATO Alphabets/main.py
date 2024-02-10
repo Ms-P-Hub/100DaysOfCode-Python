9dcfef279046478fcbaa213a31ac9ccfbe1f6399
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-10 14:06:59 +0200

    Day  30 - Added error handling

--- NATO Alphabets/main.py
+++ NATO Alphabets/main.py
@@ -1,10 +1,15 @@
 import pandas
 
-name = input("Enter Name: ").upper()
+def nato_alphabet():
+    name = input("Enter Name: ").upper()       
 
-data = pandas.read_csv("./NATO Alphabets/nato_phonetic_alphabet.csv")
+    data = pandas.read_csv("./NATO Alphabets/nato_phonetic_alphabet.csv")
 
-format_dict = {row['letter']:row['code'] for indGex, row in data.iterrows()}
+    try:
+        format_dict = {row['letter']:row['code'] for indGex, row in data.iterrows()}
+        nato = [format_dict[i] for i in name]
+        return print(nato)
+    except KeyError(""):
+        nato_alphabet()
 
-nato = [format_dict[i] for i in name]
-print(nato)
+nato_alphabet()
