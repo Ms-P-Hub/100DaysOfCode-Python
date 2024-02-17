a640aa63e2663c357b29ebe5c3803ecec4a1c2b3
gumokwe022 <124253391+Ms-P-Hub@users.noreply.github.com>
2024-02-17 16:25:39 +0200

    Day 31 - Card can flip

--- Flash Card App/main.py
+++ Flash Card App/main.py
@@ -6,24 +6,34 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("./Flash Card App/data/zulu_words.csv")
 to_learn = data.to_dict(orient="records")
+word_chosen = {}
 
 
 def word_generate():
-    row_chosen = random.choice(to_learn)
-    canvas.itemconfig(language_canvas, text="Zulu")
-    canvas.itemconfig(word_canvas, text=row_chosen["zulu"])
-    
-    
+    global word_chosen
+    canvas.itemconfig(background_image, image=front_photo)
+    word_chosen = random.choice(to_learn)
+    canvas.itemconfig(language_canvas, text="Zulu",fill="black")
+    canvas.itemconfig(word_canvas, text=word_chosen["zulu"],fill="black")
+
+
+def flip_card():
+    canvas.itemconfig(language_canvas, text="English",fill="white")
+    canvas.itemconfig(word_canvas, text=word_chosen["english"],fill="white")
+    canvas.itemconfig(background_image, image=back_photo)
+
+
 window = Tk()
 window.title("Flash Card App")
 window.config(background=BACKGROUND_COLOR, padx=50, pady=50)
-
+window.after(3000, func=flip_card)
 
 canvas = Canvas(
     width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0
 )
-photo = PhotoImage(file="./Flash Card App/images/card_front.png")
-canvas.create_image(400, 263, image=photo)
+back_photo = PhotoImage(file="./Flash Card App/images/card_back.png")
+front_photo = PhotoImage(file="./Flash Card App/images/card_front.png")
+background_image = canvas.create_image(400, 263, image=front_photo)
 language_canvas = canvas.create_text(
     400, 150, font=("Ariel", 20, "italic"), text="title"
 )
